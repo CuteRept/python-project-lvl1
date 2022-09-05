6e5599f514f8656e869e0663c3807b834edb346e
CureRept <ialsofish@gmal.com>
2022-09-05 23:36:13 +0300

    update deps

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -13,28 +13,33 @@ def main():
         random_number = random.randint(0, 100)
         print("Question:", random_number)
         answer = prompt.string("Your answer: ")
-        
         if random_number % 2 == 0:
             if answer == "yes":
                 print("Correct!")
                 i += 1
             elif answer == "no":
-                print(f"'no' is wrong answer ;(. Correct answer was 'yes'.\nLet's try again, {name}!")
-                break
+                print(f"""'no' is wrong answer ;(. Correct answer was 'yes'.
+Let's try again, {name}!""")
+                return
             else:
-                print(f"is wrong answer ;(. Correct answer was 'yes'. \nLet's try again, {name}!")
-                break
-        if random_number % 2 != 0:
+                print(f"""is wrong answer ;(. Correct answer was 'yes'.
+Let's try again, {name}!""")
+                return
+        elif random_number % 2 != 0:
             if answer == "no":
                 print("Correct!")
                 i += 1
             elif answer == "yes":
-                print(f"'yes' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, {name}!")
-                break
+                print(f"""'yes' is wrong answer ;(. Correct answer was 'no'.
+Let's try again, {name}!""")
+                return
             else:
-                print(f"is wrong answer ;(. Correct answer was 'no'. \nLet's try again, {name}!")
-                break
-    print(f'Congratulations, {name}!')
+                print(f"""is wrong answer ;(. Correct answer was 'no'.
+Let's try again, {name}!""")
+                return
+    if i == 3:
+        print(f'Congratulations, {name}!')
+
 
 if __name__ == '__main__':
     main()
